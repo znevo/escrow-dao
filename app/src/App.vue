cbbe4e19bcb74d67d2b09c3f85092cacf33e31d3
Zev Nevo <zev@nevo.io>
2022-02-19 00:16:06 -0500

    connect wallet frontend

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,5 +1,22 @@
 <template>
   <div id="app">
-    <router-view/>
+
+    <section class="hero is-success">
+      <div class="hero-body">
+
+          <div class="container is-max-desktop">
+            <p class="title">
+              EscrowDAO
+            </p>
+            <p class="subtitle">
+              Decentralized Arbiter
+            </p>
+          </div>
+
+      </div>
+    </section>
+
+    <router-view />
+
   </div>
 </template>
--- app/src/main.js
+++ app/src/main.js
@@ -2,9 +2,24 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 
+import './css/main.scss';
+
 Vue.config.productionTip = false;
 
-new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app');
+import Metamask from './library/MetaMask';
+const metamask = new Metamask();
+window.metamask = metamask;
+
+metamask.on('EVENT_ACCOUNT_CONNECTED',    () => { window.location.reload() });
+metamask.on('EVENT_ACCOUNT_SWITCHED',     () => { window.location.reload() });
+metamask.on('EVENT_ACCOUNT_DISCONNECTED', () => { window.location.reload() });
+metamask.on('EVENT_CHAIN_SWITCHED',       () => { window.location.reload() });
+
+(async() => {
+  await metamask.init();
+
+  new Vue({
+    router,
+    render: h => h(App)
+  }).$mount('#app')
+})();
