f5bd7e98d94a5735bdcd44607e5d47ffad20e4c3
Zev Nevo <zev@nevo.io>
2022-02-21 20:42:24 -0500

    env example and hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,15 @@
 require("@nomiclabs/hardhat-waffle");
+require('dotenv').config();
 
 module.exports = {
   solidity: "0.8.4",
   paths: {
     artifacts: "./app/artifacts",
+  },
+  networks: {
+    rinkeby: {
+      url: process.env.RINKEBY_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    }
   }
 };
